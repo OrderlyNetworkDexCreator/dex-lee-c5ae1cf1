428ec37a30c12b8fcf373804ab24dbd1e342f707
orderly-guoqing <guoqing@orderly.network>
2026-08-31 17:14:27 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -28,7 +28,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://pesabits.com",
+  "VITE_SEO_SITE_URL": "https://trade.pesabits.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
